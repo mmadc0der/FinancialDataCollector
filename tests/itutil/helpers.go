05a1ee0fdad84a68fccb6f42d1f9da3e7e3d9b45
mmadc0der <tim110702@mail.ru>
2025-10-23 22:54:50 +0300

    Revert "test: fix decl"
    This reverts commit ff8df0c06e77d66bbc2061dae0d1610cb99069ed.

--- tests/itutil/helpers.go
+++ tests/itutil/helpers.go
@@ -20,7 +20,6 @@ import (
 
     "github.com/example/data-kernel/internal/kernel"
     "github.com/example/data-kernel/internal/kernelcfg"
-    "github.com/example/data-kernel/internal/data"
 )
 
 // StartPostgres launches a Postgres container and returns the container handle and DSN.
